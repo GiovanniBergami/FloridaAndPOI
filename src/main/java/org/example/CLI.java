4078216739fc2e584a51d8dbfddb3cc767ef31ed
GiovanniBergami <g.bergami@studenti.unipi.it>
2024-10-25 15:53:17 +0200

    cli

--- src/main/java/org/example/CLI.java
+++ src/main/java/org/example/CLI.java
@@ -3,40 +3,104 @@ package org.example;
 import mongoConnectors.POIConnector;
 import mongoConnectors.ReviewConnector;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class CLI {
      static Scanner scanner = new Scanner(System.in);
 
     public static void start(){
-
+        boolean exit = false;
         System.out.println("Starting...");
+        while(!exit) {
+            int c = chooseBetween(List.of("Admin","User","Unsigned User","Stop"));
 
-        //parte iniziale per dividere tra unsigned user , user e admin, con autenticazione
+            switch (c) {
+                case 1:
+                    admin();
+                    break;
+                case 2:
+                    user();
+                    break;
+                case 3:
+                    unsignedUser();
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("wrong input, retry");
+            }
+        }
+    }
+    public static void admin(){
+        boolean exit = false;
+        while(!exit){
+            System.out.println("Which action do you want to take?");
+            int action = chooseBetween(List.of("Create","Remove","Update","Delete","Go back"));
+            switch(action){
+                case 1:
+                    adminInsert();
+                    break;
+                case 2:
+                    System.out.println("to do");
+                    break;
+                case 3:
+                    System.out.println("to do");
+                    break;
+                case 4:
+                    System.out.println("to do");
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("wrong input, retry");
+            }
+        }
 
+    }
+    public static void user(){
 
+    }
+    public static void unsignedUser(){
 
-        //cose messe ora per provare
-        System.out.println("Insert POIs (1) / Insert Users (2) / Insert Reviews (3)");
+    }
 
-        String activity = scanner.nextLine();
-
+    public static void adminInsert(){
+        int activity = chooseBetween(List.of("Create POIs","Create Users","Create Reviews"," go back"));
+        String path;
         switch(activity){
-            case "1":
-                System.out.println("Insert POIs");
-                POIConnector.insertPOIs("dataset/florida_business_yelp.json"); //questo verrà eventualmente inserito da tastiera
+            case 1:
+                System.out.println("Insert POIs, specify path");
+                path = scanner.nextLine();
+                POIConnector.insertPOIs(path); //questo verrà eventualmente inserito da tastiera
                 POIConnector.count();
                 break;
-            case "2":
+            case 2:
                 System.out.println("Insert Users");
                 break;
-            case "3":
-                System.out.println("Insert Reviews");
-                ReviewConnector.insertReviews("dataset/review_florida_reduced.json");
+            case 3:
+                System.out.println("Insert Reviews, specify path");
+                path = scanner.nextLine();
+                ReviewConnector.insertReviews(path);
                 ReviewConnector.count();
                 ReviewConnector.printCollection(3);
                 break;
+            case 4:
+                break;
+            default:
+                System.out.println("wrong input, retry");
         }
+    }
+
+    public static int chooseBetween(List<String> options){
+        int i = 1;
+        for(String option : options){
+            System.out.println(" "+i+" - "+option);
+            i++;
+        }
+        return scanner.nextInt();
     }
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,24 @@
 package org.example;
 
 import mongoConnectors.mongoConnector;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
+import neoConnectors.neoConnector;
 
 public class Main {
     public static void main(String[] args) {
 
 //        neoConnector.connect();
-//        CLI.start();
-//        mongoConnector.close();
-
-//        Terminal terminal = TerminalBuilder.terminal();
-//        LineReader lineReader = LineReaderBuilder.builder()
-//                .terminal(terminal)
-//                .build();
-//
-//        String line = lineReader.readLine("prompt> ");
+        CLI.start();
+        mongoConnector.close();
 
 
     }
 }
+
+
+// cli con jline
+//    Terminal terminal = TerminalBuilder.terminal();
+//    LineReader lineReader = LineReaderBuilder.builder()
+//            .terminal(terminal)
+//            .build();
+//
+//    String line = lineReader.readLine("prompt> ");
