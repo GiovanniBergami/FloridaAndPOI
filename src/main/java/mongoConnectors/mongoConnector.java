a10a45a554c9aef1fc8192ba2382b91a43a426fe
GiovanniBergami <g.bergami@studenti.unipi.it>
2024-11-21 18:35:21 +0100

    support distributed systems

--- src/main/java/mongoConnectors/mongoConnector.java
+++ src/main/java/mongoConnectors/mongoConnector.java
@@ -1,11 +1,26 @@
 package mongoConnectors;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadPreference;
+import com.mongodb.WriteConcern;
 import com.mongodb.client.*;
 import org.bson.Document;
 
 public class mongoConnector {
-    static public MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-    static public MongoDatabase database =  mongoClient.getDatabase("LargeScaleProject");
+//    static public MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+//    static public MongoDatabase database =  mongoClient.getDatabase("LargeScaleProject");
+
+    static ConnectionString uri = new ConnectionString("mongodb://localhost:27018");
+    static MongoClientSettings mcs = MongoClientSettings.builder()
+            .applyConnectionString(uri)
+            .readPreference(ReadPreference.nearest())
+            .retryWrites(true)
+            .writeConcern(WriteConcern.ACKNOWLEDGED).build();
+    static public MongoClient mongoClient = MongoClients.create(mcs);
+
+    //sopra nuovo modo
+    static public MongoDatabase database =  mongoClient.getDatabase("LargeScale");
 
     public static void close(){
         mongoClient.close();
